c9465656f718196d62f9fa3ffa0035a862b97e9c
svone <serjvolk222@gmail.com>
2025-04-03 08:47:33 +0300

    gamemania

--- gamemania/docs/js/script.js
+++ gamemania/docs/js/script.js
@@ -1,63 +1,64 @@
-$(document).ready(function () {
-	var w = $(window).outerWidth();
-	var h = $(window).outerHeight();
-	var isMobile = ('ontouchstart' in window);
-	const $body = $('body');
-	const BREAKPOINT_md1 = 1343;
-	const BREAKPOINT_1045 = 1044.98;
-	const BREAKPOINT_md2 = 992.98;
-	const BREAKPOINT_872 = 871.98;
-	const BREAKPOINT_md3 = 767.98;
-	const BREAKPOINT_552 = 551.98;
-	const BREAKPOINT_md4 = 479.98;
+// /* Управление меню */
+(function() {
+	const burgerBtn = document.querySelector('.js-burger-btn');
+	const burgerMenu = document.querySelector('.js-burger-menu');
+	const bgOverlay = document.querySelector('.js-bgOverlay');
 
-	$(window).scroll(function() {
-        let scrollTop = $(this).scrollTop();
-        scrollForBtnScrollTop(scrollTop);
-    });
+	if (burgerBtn && burgerMenu && bgOverlay) {
+		// Открыть меню
+		burgerBtn.addEventListener('click', () => {
+			document.body.classList.add('lock');
+			burgerMenu.classList.add('active');
+			bgOverlay.classList.add('active');
+		});
 
-	// Управление меню | Открыть/закрыть
-    (function() {
-        if(w < BREAKPOINT_md3){
-            // Открыть меню
-            $('.js-burger-btn').click(function(){
-                $('body').addClass('lock');
-                $('.js-burger-menu').addClass('active');
-				$('.js-bgOverlay').addClass('active');
-            });
-
-            // Закрыть меню
-            $('.js-bgOverlay, .js-burger-close').click(function(e){
-                e.stopPropagation();
-                $('body').removeClass('lock');
-                $('.js-burger-menu').removeClass('active');
-				$('.js-bgOverlay').removeClass('active');
-            });
-        }
-    })();
-
-	// Показать/Скрыть кнопку прокрутки "Вверх"
-	function scrollForBtnScrollTop(scrollTop){
-        if (scrollTop > 500) {
-            $('.js-btnScrollToTop').show();
-        } else {
-            $('.js-btnScrollToTop').hide();
-        }
+		// Закрыть меню
+		bgOverlay.addEventListener('click', (e) => {
+			e.stopPropagation();
+			document.body.classList.remove('lock');
+			burgerMenu.classList.remove('active');
+			bgOverlay.classList.remove('active');
+		});
 	}
+})();
 
-	// Прокрутить вверх
-    $('.js-btnScrollToTop').click(function() {
-        $('html, body').animate({ scrollTop: 0 }, 'smooth');
-        return false;
-    });
-
-	function ibg(){ // Превращает картинку img в background-image
-		let ibg=document.querySelectorAll(".ibg");
-		for (var i = 0; i < ibg.length; i++) {
-			if(ibg[i].querySelector('img')){
-				ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
-			}
-		}
-	}
-	ibg();
+/* Кнопка прокрутки вверх */
+window.addEventListener('scroll', () => {
+    const scrollTop = window.scrollY;
+    const btnScrollToTop = document.querySelector('.js-btnScrollToTop');
+	
+    if (btnScrollToTop) {
+        btnScrollToTop.style.display = scrollTop > 500 ? 'block' : 'none';
+    }
 });
+
+/** dropdown */
+(function () {
+    // Клик по выпадающему списку
+    document.addEventListener("click", (event) => {
+        const dropdownHead = event.target.closest(".js-dropdown-head");
+
+        if (!dropdownHead) return;
+
+        const dropdownItem = dropdownHead.closest(".js-dropdown-item");
+
+        if (!dropdownItem) return;
+
+        const dropdownBody = dropdownItem.querySelector(".js-dropdown-body");
+        if (!dropdownBody) return;
+
+        const isActive = dropdownItem.classList.contains("active");
+
+        dropdownItem.classList.toggle("active", !isActive);
+        dropdownBody.style.display = isActive ? "none" : "block";
+    });
+
+    /** При инициализации страницы закрываем/открываем выпадающий список в зависимости от класса "active" */
+    document.querySelectorAll(".js-dropdown-item").forEach((dropdownItem) => {
+        const dropdownBody = dropdownItem.querySelector(".js-dropdown-body");
+        if (!dropdownBody) return;
+
+        const isActive = dropdownItem.classList.contains("active");
+        dropdownBody.style.display = isActive ? "block" : "none";
+    });
+})();
